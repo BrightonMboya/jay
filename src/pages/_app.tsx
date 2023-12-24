e60c9dd902fdd348bf53df67a98aa99b88ae6448
brightonmboya <b.mboya@alustudent.com>
2023-12-24 09:54:03 +0200

    added fonts and initial ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,20 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
