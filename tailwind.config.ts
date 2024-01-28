7d7880eb1f6b256be04d46579522d109df54dbb5
brightonmboya <b.mboya@alustudent.com>
2024-01-28 16:13:39 +0200

    added colors on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,8 +22,31 @@ export default {
         lighter: "#dcc6ad",
         light: "#c29c70",
         lightest: "#f6f1eb",
-        muted: "hsl(var(--muted-foreground))",
-       foreground: "hsl(var(--foreground))"
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
     },
   },
