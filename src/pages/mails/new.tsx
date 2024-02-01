7d0207f0eb007f0bf390f26f1e7659373439398a
brightonmboya <b.mboya@alustudent.com>
2024-02-01 22:34:28 +0200

    create the email succesful

--- src/pages/mails/new.tsx
+++ src/pages/mails/new.tsx
@@ -10,6 +10,11 @@ import { NextPageWithLayout } from "../_app";
 import { ReactElement } from "react";
 import { api } from "~/utils/api";
 import { useUser } from "@clerk/nextjs";
+import { ToastAction } from "~/components/ui/Toast";
+import { useToast } from "~/utils/hooks/useToast";
+import LoadingSkeleton from "~/components/trips/LoadingSkeleton";
+import { useRouter } from "next/router";
+import { Toaster } from "~/components/ui/toaster";
 
 export const mailSchema = z.object({
   name: z.string().min(1),
@@ -27,8 +32,23 @@ const Page: NextPageWithLayout = () => {
   } = useForm<MailValidationSchema>({
     resolver: zodResolver(mailSchema),
   });
+  const router = useRouter();
+  const { toast } = useToast();
 
-  const { mutateAsync } = api.createMail.create.useMutation();
+  const { mutateAsync, isLoading } = api.createMail.create.useMutation({
+    onSuccess: () => {
+      router.push("/mails");
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: `${error.message}`,
+        action: <ToastAction altText="Try again">Try again</ToastAction>,
+        duration: 1500,
+      });
+    },
+  });
   const user = useUser();
 
   const onSubmit: SubmitHandler<MailValidationSchema> = async (data) => {
@@ -39,46 +59,49 @@ const Page: NextPageWithLayout = () => {
     });
   };
 
-  // console.log(errors);
   return (
     <main className="mt-[50px]">
       <h1 className="text-xl font-bold">Add New Email Template</h1>
+      <Toaster />
+      {isLoading ? (
+        <LoadingSkeleton />
+      ) : (
+        <form
+          className="relative  mt-10 flex flex-col space-y-[30px] "
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <ItemLayout>
+            <AssetLabel
+              label="Email Name"
+              caption="Give your email a unique email"
+            />
+            <Input placeholder="Onboarding Email" {...register("name")} />
+          </ItemLayout>
 
-      <form
-        className="relative  mt-10 flex flex-col space-y-[30px] "
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <ItemLayout>
-          <AssetLabel
-            label="Email Name"
-            caption="Give your email a unique email"
-          />
-          <Input placeholder="Onboarding Email" {...register("name")} />
-        </ItemLayout>
+          <ItemLayout>
+            <AssetLabel
+              label="Tags"
+              caption="Tags easily allow you to search through your email templates"
+            />
+            <Input placeholder="pre-trip" {...register("label")} />
+          </ItemLayout>
 
-        <ItemLayout>
-          <AssetLabel
-            label="Tags"
-            caption="Tags easily allow you to search through your email templates"
-          />
-          <Input placeholder="pre-trip" {...register("label")} />
-        </ItemLayout>
-
-        <ItemLayout>
-          <AssetLabel
-            label="body"
-            caption="Enter the content of your email here"
-          />
-          <Textarea
-            placeholder="Hello John, thank you for choosing to travel with us ..."
-            rows={15}
-            {...register("body")}
-          />
-        </ItemLayout>
-        <Button className="mt-[50px] w-full" type="submit">
-          Add Email
-        </Button>
-      </form>
+          <ItemLayout>
+            <AssetLabel
+              label="body"
+              caption="Enter the content of your email here"
+            />
+            <Textarea
+              placeholder="Hello John, thank you for choosing to travel with us ..."
+              rows={15}
+              {...register("body")}
+            />
+          </ItemLayout>
+          <Button className="mt-[50px] w-full" type="submit">
+            Add Email
+          </Button>
+        </form>
+      )}
     </main>
   );
 };
