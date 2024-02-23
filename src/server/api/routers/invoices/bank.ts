75987a6900b5170d56e82353531c3e91ec982a42
brightonmboya <b.mboya@alustudent.com>
2024-02-23 20:58:42 +0200

    added the organization bank details

--- src/server/api/routers/invoices/bank.ts
+++ src/server/api/routers/invoices/bank.ts
@@ -1,12 +1,59 @@
 import { createTRPCRouter, protectedProcedure } from "../../trpc";
-import {z} from "zod";
+import { z } from "zod";
+import { bankSchema } from "~/components/invoices/bankForms/CreateBank";
+import { CANT_MUTATE_ERROR } from "../trips/newTrip";
 
 export const bank = createTRPCRouter({
-    create: protectedProcedure
-    .input()
-    .mutation(async ({ctx, input}) => {
-        try {
-            const newBank = await ctx.db.
-        }
-    })
-})
+  create: protectedProcedure
+    .input(
+      bankSchema.merge(
+        z.object({
+          organizationEmail: z.string(),
+        }),
+      ),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const organizationId = await ctx.db.organizations.findUnique({
+          where: {
+            emailAddress: input.organizationEmail,
+          },
+        });
+        const newBank = await ctx.db.bankDetails.create({
+          data: {
+            ...input,
+            organizationId: Number(organizationId?.id),
+          },
+        });
+        return newBank;
+      } catch (cause) {
+        console.log(cause);
+        throw CANT_MUTATE_ERROR;
+      }
+    }),
+
+  byOrganization: protectedProcedure
+    .input(
+      z.object({
+        organizationEmail: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const organizationId = await ctx.db.organizations.findUnique({
+        where: {
+          emailAddress: input.organizationEmail,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      const organizationBanks = await ctx.db.bankDetails.findMany({
+        where: {
+          organizationId: organizationId?.id,
+        },
+      });
+
+      return organizationBanks;
+    }),
+});
--- src/server/api/routers/invoices/index.ts
+++ src/server/api/routers/invoices/index.ts
@@ -2,5 +2,6 @@ import { mergeRouters } from "../../trpc";
 import { create } from "./create";
 import { edit } from "./edit";
 import { query } from "./query";
+import { bank } from "./bank";
 
-export const invoices = mergeRouters(create, edit, query);
+export const invoices = mergeRouters(create, edit, query, bank);
